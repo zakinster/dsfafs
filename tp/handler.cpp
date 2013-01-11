4ac0c0878784f7dd37c344217c6022275dfe65e9
zakinster <zakinster@45064hpv114161.ikoula.com>
2013-01-11 23:21:52 +0100

    Added C++ handler

---
+++ tp/handler.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+	cout << "Content-Type: text/plain"<< endl << endl;
+	cout << "Hello world!" << endl << endl;
+	return 0;
+}
